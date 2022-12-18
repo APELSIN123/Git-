8d517a7ded108ec5a29a00f54067a8dd9724bd76
Апельсин <apelsin15022005@mail.ru>
2022-12-18 18:46:24 +0900

    дз коммит

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-def my_superfunction():
-    print('tyt')
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+import sys
+import random
 
 
-def main():
-    print('Again')
-    print('Again')
-    print('UFO came and added this line')
-    my_superfunction()
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.flag = False
+        self.setWindowTitle('Git и желтые окружности')
+        self.btn_circle.clicked.connect(self.draw)
+        self.coords = []
+
+    def draw(self):
+        self.size = random.randint(10, 100)
+        self.color = (255, 255, 0)
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setPen(QColor(*self.color))
+            qp.setBrush(QColor(*self.color))
+            self.x, self.y = random.randint(100, 600), random.randint(100, 400)
+            qp.drawEllipse(self.x, self.y, self.size, self.size)
+            qp.end()
+
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
