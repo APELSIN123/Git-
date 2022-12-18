8968f5713ac31e236d0af372587d48a11973928b
Апельсин <apelsin15022005@mail.ru>
2022-12-18 19:00:14 +0900

    дз коммит 2

--- main.py
+++ main.py
@@ -3,12 +3,42 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 import sys
 import random
+from PyQt5 import QtCore, QtWidgets
 
 
-class Example(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(700, 500)
+        MainWindow.setMinimumSize(QtCore.QSize(700, 500))
+        MainWindow.setMaximumSize(QtCore.QSize(700, 500))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.btn_circle = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_circle.setGeometry(QtCore.QRect(280, 410, 151, 41))
+        self.btn_circle.setObjectName("btn_circle")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 700, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.btn_circle.setText(_translate("MainWindow", "Создать круг"))
+
+
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.flag = False
         self.setWindowTitle('Git и желтые окружности')
         self.btn_circle.clicked.connect(self.draw)
@@ -16,7 +46,7 @@ class Example(QMainWindow):
 
     def draw(self):
         self.size = random.randint(10, 100)
-        self.color = (255, 255, 0)
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.flag = True
         self.update()
 
